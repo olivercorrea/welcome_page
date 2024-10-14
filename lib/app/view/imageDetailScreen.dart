0978474f98d169ceebb0ba20621ba7a8e028b1f5
olivercorrea <olivercorreacabana@outlook.es>
2024-10-14 09:42:27 -0500

    first commit

---
+++ lib/app/view/imageDetailScreen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ImageDetailScreen extends StatefulWidget {
+  final String image;
+  final String description;
+  final String details;
+
+  const ImageDetailScreen({
+    super.key,
+    required this.image,
+    required this.description,
+    required this.details,
+  });
+
+  @override
+  State<ImageDetailScreen> createState() => _ImageDetailScreenState();
+}
+
+class _ImageDetailScreenState extends State<ImageDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.description),
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: Image.asset(
+              widget.image,
+              width: 200,
+              height: 200,
+            ),
+          ),
+          const SizedBox(height: 10),
+          Expanded(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                widget.details,
+                textAlign: TextAlign.justify,
+                style: GoogleFonts.oregano(
+                  textStyle: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:welcome_page/app/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
